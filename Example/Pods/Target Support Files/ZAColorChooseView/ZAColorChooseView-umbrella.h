185618608d5972d4189a7fdc1dbc53a5ee4cf836
chuting <chuting@zhuoapp.com>
2016-09-07 14:11:07 +0800

    modify

---
+++ Example/Pods/Target Support Files/ZAColorChooseView/ZAColorChooseView-umbrella.h
@@ -0,0 +1,9 @@
+#import <UIKit/UIKit.h>
+
+#import "HSV.h"
+#import "ZAColorChooseView.h"
+#import "ZAColorFullChooseView.h"
+
+FOUNDATION_EXPORT double ZAColorChooseViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char ZAColorChooseViewVersionString[];
+
